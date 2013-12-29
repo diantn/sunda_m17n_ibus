e4a5a078faa02788b29ac4b1b2aeffd56f0bcfcf
Dian Tresna Nugraha <dian.nugraha@gmail.com>
2013-12-29 20:40:38 +0100

    updated with messages.

--- install_all.sh
+++ install_all.sh
@@ -1,5 +1,13 @@
 sudo apt-get install ibus-m17n
 sudo apt-get install m17n-im-config
+echo 'Removing old layout files'
+cp /usr/share/m17n/su-inscript.mim su-inscript.mim.bak 
+cp /usr/share/m17n/su-phonetic.mim su-phonetic.mim.bak
+rm /usr/share/m17n/su-inscript.mim 
+rm /usr/share/m17n/su-phonetic.mim 
+echo 'Copying new layout files'
 sudo cp su-inscript.mim /usr/share/m17n/
 sudo cp su-phonetic.mim /usr/share/m17n/
+echo 'Refreshing IBus daemon'
 ibus-daemon -drx
+echo 'Done. Please go to the "Text Entry" and choose the Sundanese inputs'
