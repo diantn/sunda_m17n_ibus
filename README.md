d9a9520897d35be53efd3cde614a581b422986cb
Dian Tresna Nugraha <dian.nugraha@gmail.com>
2013-12-29 21:30:53 +0100

    Added README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+These are scripts for Sundanese Unicode input method in Ubuntu-12.04 till Ubuntu 13.10
+
+To install font from Terminal:
+    sh install_font.sh
+
+To install input method from Terminal:
+    sh install_im.sh
+    
+Then the input method can be activated using the "IBus preferences" or "Text Entry" settings
--- install_font.sh
+++ install_font.sh
@@ -1,11 +1,9 @@
 echo 'Installing wget'
 sudo apt-get install wget
 
-echo 'Downloading font'
+echo 'Copying font'
 mkdir ~/.fonts
 rm ~/.fonts/SundaneseUnicode*.ttf
-#rm ~/SundaneseUnicode*.ttf
-#wget http://diantn.github.io/SundaConv/SundaneseUnicode-1.0.6.ttf
 cp SundaneseUnicode-1.0.6.ttf ~/.fonts/
 
 echo 'Refreshing font cache'
